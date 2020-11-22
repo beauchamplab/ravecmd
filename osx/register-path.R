83b1c90b35bc9b46d2604e362dea1c59cebd010b
dipterix <zw23@rice.edu>
2020-11-22 14:36:13 -0600

    bug fix

--- osx/register-path.R
+++ osx/register-path.R
@@ -4,6 +4,7 @@ add_sh_profile <- function(path, cmd = 'export PATH=$PATH:/Applications/RAVE/bin
   # path <- '.profile'
   cat("\nAdd RAVE command-line tools to -", path, end = "")
   f <- file.path('~/', path)
+  s <- ''
   if(file.exists(f)){
     s <- readLines(f)
     if(any(s == cmd)) {
@@ -17,12 +18,9 @@ add_sh_profile <- function(path, cmd = 'export PATH=$PATH:/Applications/RAVE/bin
 }
 
 # Add files to commandline start-up scripts
-print('1')
 add_sh_profile('.profile')
 add_sh_profile('.bash_profile')
 add_sh_profile('.zshenv')
-print('2')
 add_sh_profile('.cshrc', 'set path = ( $path /Applications/RAVE/bin )')
 add_sh_profile('.tcshrc', 'set path = ( $path /Applications/RAVE/bin )')
-print('3')
 cat("\nDone.")
